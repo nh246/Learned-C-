d8dff9e6b836fd9de27d21a874add4c884730169
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2023-09-28 23:16:08 +0600

    Create Classandobj.cpp

---
+++ Classandobj.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+// Define a class called "Car"
+class Car {
+public:
+    // Properties (attributes)
+    string brand;
+    string model;
+    int year;
+
+    // Methods (functions)
+    void start() {
+        cout << "Starting the " << brand << " " << model << "...\n";
+    }
+
+    void accelerate() {
+        cout << "Accelerating...\n";
+    }
+};
+
+int main() {
+    // Create objects (instances) of the "Car" class
+    Car car1;
+    car1.brand = "Toyota";
+    car1.model = "Camry";
+    car1.year = 2020;
+
+    Car car2;
+    car2.brand = "Ford";
+    car2.model = "Mustang";
+    car2.year = 2021;
+
+    // Use the objects
+    cout << "Car 1: " << car1.year << " " << car1.brand << " " << car1.model << endl;
+    car1.start();
+    car1.accelerate();
+
+    cout << "\nCar 2: " << car2.year << " " << car2.brand << " " << car2.model << endl;
+    car2.start();
+    car2.accelerate();
+
+    return 0;
+}
