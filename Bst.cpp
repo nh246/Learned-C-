2d4753c6f84320b647b7446512908c6b04c6f9ee
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2024-02-26 22:28:56 +0600

    Create Bst.cpp

---
+++ Bst.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+    if (n == 1) {
+        std::cout << "Move disk 1 from " << source << " to " << destination << std::endl;
+        return;
+    }
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+    std::cout << "Move disk " << n << " from " << source << " to " << destination << std::endl;
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+int main() {
+    int num_disks = 3; // Change the number of disks here
+    towerOfHanoi(num_disks, 'A', 'B', 'C');
+    return 0;
+}
