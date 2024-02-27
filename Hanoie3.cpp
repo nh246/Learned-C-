fc6d0033f5c6a250d7cb1d9cc267cb234dbc8024
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2024-02-27 20:29:26 +0600

    Create Hanoie3.cpp

---
+++ Hanoie3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+class TowerOfHanoi {
+private:
+    std::vector<std::stack<int>> pegs;
+    int num_disks;
+
+public:
+    TowerOfHanoi(int n) : num_disks(n) {
+        // Initialize the pegs
+        for (int i = 0; i < 3; ++i) {
+            std::stack<int> s;
+            pegs.push_back(s);
+        }
+
+        // Add disks to the first peg
+        for (int i = num_disks; i >= 1; --i) {
+            pegs[0].push(i);
+        }
+    }
+
+    void moveDisks(int n, int source, int auxiliary, int destination) {
+        if (n > 0) {
+            moveDisks(n - 1, source, destination, auxiliary);
+            int disk = pegs[source].top();
+            pegs[source].pop();
+            pegs[destination].push(disk);
+            std::cout << "Move disk " << disk << " from Peg " << source + 1 << " to Peg " << destination + 1 << std::endl;
+            moveDisks(n - 1, auxiliary, source, destination);
+        }
+    }
+
+    void solve() {
+        moveDisks(num_disks, 0, 1, 2);
+    }
+};
+
+int main() {
+    int num_disks = 3; // Change the number of disks here
+    TowerOfHanoi tower(num_disks);
+    tower.solve();
+    return 0;
+}
