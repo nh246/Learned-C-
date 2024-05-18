c4e3b3ce8a945d0443eee9464f7ed4ca69d1757a
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2024-05-18 22:19:13 +0600

    Create floyd.c

---
+++ floyd.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define INF 99999
+#define V 4
+
+void floydWarshall(int graph[][V]) {
+    int dist[V][V];
+    int i, j, k;
+
+    // Initialize distance matrix
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    // Update distance matrix
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    // Print the shortest distances
+    printf("Shortest distances between every pair of vertices:\n");
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("INF\t");
+            else
+                printf("%d\t", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 5, INF, 10},
+        {INF, 0, 3, INF},
+        {INF, INF, 0, 1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+    return 0;
+}
