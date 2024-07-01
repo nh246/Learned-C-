f3930da2eaf688c388f757993b326421e3c87ca6
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2024-07-01 21:42:51 +0600

    Update Arrayiterate.cpp

--- Arrayiterate.cpp
+++ Arrayiterate.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
 int main() {
-    // Define an array
+    
     int myArray[] = {1, 2, 3, 4, 5};
-
-    // Calculate the size of the array
     int arraySize = sizeof(myArray) / sizeof(myArray[0]);
 
-    // Iterate through the array and print each element
+    
     for (int i = 0; i < arraySize; ++i) {
         std::cout << "Element at index " << i << ": " << myArray[i] << std::endl;
     }
