79fe6b97c241311cd317a51f6f15044cbbd23083
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2024-07-09 22:03:13 +0600

    Update Noninteger.cpp

--- Noninteger.cpp
+++ Noninteger.cpp
@@ -4,6 +4,9 @@ int main() {
     const int size = 5; 
     int arr[size] = {1, 2, 3, 4, 5}; 
 
+
+
+    
     std::cout << "Non-integer elements in the array: ";
 
     for (int i = 0; i < size; ++i) {
