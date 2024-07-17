d74b33cbb28974ff1fbe92a27d2d869931c4c460
Nazmul Hossain <129146524+nh246@users.noreply.github.com>
2024-07-17 22:49:16 +0600

    Update asskds.c

--- asskds.c
+++ asskds.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <limits.h>
 
-#define V 4 // Number of vertices in the graph
+#define V 4 
 
-// Function to print the solution matrix
+
 void printSolution(int dist[][V]) {
     printf("Shortest distances between all pairs of vertices:\n");
     for (int i = 0; i < V; i++) {
@@ -18,24 +18,24 @@ void printSolution(int dist[][V]) {
     }
 }
 
-// Function to implement Floyd-Warshall algorithm for finding shortest paths between all pairs of vertices
+
 void floydWarshall(int graph[][V]) {
     int dist[V][V];
 
-    // Initialize the solution matrix same as input graph matrix
+    
     for (int i = 0; i < V; i++) {
         for (int j = 0; j < V; j++) {
             dist[i][j] = graph[i][j];
         }
     }
 
-    // Update dist[][] to store the shortest distance between every pair of vertices
+    
     for (int k = 0; k < V; k++) {
-        // Pick all vertices as source one by one
+        
         for (int i = 0; i < V; i++) {
-            // Pick all vertices as destination for the above picked source
+            
             for (int j = 0; j < V; j++) {
-                // If vertex k is on the shortest path from i to j, then update the value of dist[i][j]
+                
                 if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] + dist[k][j] < dist[i][j]) {
                     dist[i][j] = dist[i][k] + dist[k][j];
                 }
